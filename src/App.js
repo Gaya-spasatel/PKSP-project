6180bd471c378262df837752b2267c4738526726
Gaya-spasatel <dashapereg@mail.ru>
2022-03-04 14:33:23 +0300

    Header done for pr2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Header/>
-        <img src="./components/photo_2021-02-08_19-01-22.jpg" alt="фото"/>
         <Footer/>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import './Header.css'
-import logo from "./logo.svg"
+import logo from "./images/logo.svg"
 
 function Header(){
     return(
         <header>
             <div className='header'>
             <ul className="header_menu">
-                <li className='header_logo'><a href=""><img src={logo} alt="logo"/></a></li>
+                <li><a href="" className='header_logo'><img src={logo} alt="logo"/></a></li>
                 <li><a href="">Пункт меню</a></li>
                 <li><a href="">Пункт меню</a></li>
                 <li><a href="">Пункт меню</a></li>
