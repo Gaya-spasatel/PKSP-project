db0eae94308e89dfba90edfff0996085556700c5
Gaya-spasatel <dashapereg@mail.ru>
2022-03-06 17:04:30 +0300

    Posts navigation

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,5 +1,6 @@
 import "./Post.css"
 import UserInfo from "./UserInfo";
+import {Link} from "react-router-dom";
 
 function formdate(date) {
     return date.toLocaleDateString();
@@ -8,7 +9,7 @@ function formdate(date) {
 function Post(props){
     return(
         <div className="Post">
-            <div className="Post-name">{props.name}</div>
+            <Link to={`/posts/${props.id}`} className="Post-name">{props.name}</Link>
             <UserInfo user={props.author}/>
             <div className="Post-text">{props.text}</div>
             <div className="Post-date">{formdate(props.date)}</div>
