a175140761945164efe4dd26fcf56da5e1e69bb6
Gaya-spasatel <dashapereg@mail.ru>
2022-04-30 10:58:37 +0300

    access for only auth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
 import ContentPosts from "./components/content/ContentPosts";
 import ContentPost from "./components/content/ContentPost";
 import Authorization from "./components/auth_reg/Authorization";
 import Registration from "./components/auth_reg/Registration";
 import {register, login} from "./store/actions";
-import {connect} from "react-redux";
+import {connect, useSelector} from "react-redux";
 import {bindActionCreators} from "redux";
 import ContentPostsMy from "./components/content/ContentPostsMy";
 import ContentAddPost from "./components/content/ContentAddPost";
+import {isAuthenticated} from "./store/selectors";
 
 const post_first = {
-    id:0,
+    id: 0,
     date: new Date(),
     name: "First post",
     text: "Повседневная практика показывает, что разбавленное изрядной долей эмпатии, рациональное мышление напрямую зависит от форм воздействия. Имеется спорная точка зрения, гласящая примерно следующее: предприниматели в сети интернет освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, подвергнуты целой серии независимых исследований. Безусловно, социально-экономическое развитие не даёт нам иного выбора, кроме определения глубокомысленных рассуждений.",
@@ -24,8 +25,8 @@ const post_first = {
     },
 }
 
-const post_second= {
-    id:1,
+const post_second = {
+    id: 1,
     date: new Date(),
     name: "Second post",
     text: "Повседневная практика показывает, что разбавленное изрядной долей эмпатии, рациональное мышление напрямую зависит от форм воздействия. Имеется спорная точка зрения, гласящая примерно следующее: предприниматели в сети интернет освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, подвергнуты целой серии независимых исследований. Безусловно, социально-экономическое развитие не даёт нам иного выбора, кроме определения глубокомысленных рассуждений.",
@@ -38,21 +39,24 @@ const post_second= {
 const posts = [post_first, post_second]
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-        <Routes>
-            <Route exact path='/' element={<Home/>}/>
-            <Route path='/posts/all' element={<ContentPosts posts={posts}/>}/>
-            <Route path='/posts/:number' element={<ContentPost posts={posts}/>}/>
-            <Route path='/auth' element={<Authorization onSubmit={login}/>}/>
-            <Route path='/reg' element={<Registration onSubmit={register}/>}/>
-            <Route path='/posts/my' element={<ContentPostsMy/>}/>
-            <Route path='/posts/add' element={<ContentAddPost/>}/>
-        </Routes>
-        <Footer/>
-    </div>
-  );
+    const auth = useSelector(isAuthenticated());
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                {!auth && <Route path={"*"} element={<Navigate replace to="/auth"/>}/>}
+                {auth && <Route exact path='/' element={<Home/>}/>}
+                {auth && <Route path='/posts/all' element={<ContentPosts posts={posts}/>}/>}
+                {auth && <Route path='/posts/:number' element={<ContentPost posts={posts}/>}/>}
+                <Route path='/auth' element={<Authorization onSubmit={login}/>}/>
+                <Route path='/reg' element={<Registration onSubmit={register}/>}/>
+                {auth && <Route path='/posts/my' element={<ContentPostsMy/>}/>}
+                {auth && <Route path='/posts/add' element={<ContentAddPost/>}/>}
+
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({
